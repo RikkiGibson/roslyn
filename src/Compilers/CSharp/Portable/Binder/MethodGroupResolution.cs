e831475c72821ad4e3b28844806d0799ca40b8a3
Rikki Gibson <rikkigibson@gmail.com>
2026-07-01 19:40:27 -0700

    Nullable-enable MethodGroupResolution.cs

--- src/Compilers/CSharp/Portable/Binder/MethodGroupResolution.cs
+++ src/Compilers/CSharp/Portable/Binder/MethodGroupResolution.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
@@ -18,10 +16,10 @@ namespace Microsoft.CodeAnalysis.CSharp
     /// </summary>
     internal readonly struct MethodGroupResolution
     {
-        public readonly MethodGroup MethodGroup;
-        public readonly Symbol OtherSymbol;
-        public readonly OverloadResolutionResult<MethodSymbol> OverloadResolutionResult;
-        public readonly AnalyzedArguments AnalyzedArguments;
+        public readonly MethodGroup? MethodGroup;
+        public readonly Symbol? OtherSymbol;
+        public readonly OverloadResolutionResult<MethodSymbol>? OverloadResolutionResult;
+        public readonly AnalyzedArguments? AnalyzedArguments;
         public readonly ReadOnlyBindingDiagnostic<AssemblySymbol> Diagnostics;
         public readonly LookupResultKind ResultKind;
 
@@ -36,18 +34,18 @@ namespace Microsoft.CodeAnalysis.CSharp
         }
 
         public MethodGroupResolution(
-            MethodGroup methodGroup,
-            Symbol otherSymbol,
-            OverloadResolutionResult<MethodSymbol> overloadResolutionResult,
-            AnalyzedArguments analyzedArguments,
+            MethodGroup? methodGroup,
+            Symbol? otherSymbol,
+            OverloadResolutionResult<MethodSymbol>? overloadResolutionResult,
+            AnalyzedArguments? analyzedArguments,
             LookupResultKind resultKind,
             ReadOnlyBindingDiagnostic<AssemblySymbol> diagnostics)
         {
-            Debug.Assert((methodGroup != null) || ((object)otherSymbol != null) || analyzedArguments == null); // analyzedArguments is only set if we have some result
+            Debug.Assert((methodGroup != null) || (otherSymbol is not null) || analyzedArguments == null); // analyzedArguments is only set if we have some result
             Debug.Assert((methodGroup == null) || (methodGroup.Methods.Count > 0));
-            Debug.Assert((methodGroup == null) || ((object)otherSymbol == null));
+            Debug.Assert((methodGroup == null) || (otherSymbol is null));
             // Methods should be represented in the method group.
-            Debug.Assert(((object)otherSymbol == null) || (otherSymbol.Kind != SymbolKind.Method));
+            Debug.Assert((otherSymbol is null) || (otherSymbol.Kind != SymbolKind.Method));
             Debug.Assert(resultKind != LookupResultKind.Ambiguous); // HasAnyApplicableMethod is expecting Viable methods.
             Debug.Assert(!diagnostics.Diagnostics.IsDefault);
             Debug.Assert(!diagnostics.Dependencies.IsDefault);
@@ -62,7 +60,7 @@ namespace Microsoft.CodeAnalysis.CSharp
 
         public bool IsEmpty
         {
-            get { return (this.MethodGroup == null) && ((object)this.OtherSymbol == null); }
+            get { return (this.MethodGroup == null) && (this.OtherSymbol is null); }
         }
 
         public bool HasAnyErrors
@@ -70,6 +68,7 @@ namespace Microsoft.CodeAnalysis.CSharp
             get { return this.Diagnostics.Diagnostics.HasAnyErrors(); }
         }
 
+        [MemberNotNullWhen(true, nameof(MethodGroup))]
         public bool HasAnyApplicableMethod
         {
             get
@@ -85,7 +84,6 @@ namespace Microsoft.CodeAnalysis.CSharp
             get { return (this.MethodGroup != null) && this.MethodGroup.IsExtensionMethodGroup; }
         }
 
-#nullable enable
         /// <summary>
         /// Indicates that we have a viable result that is a non-method extension member.
         /// </summary>
@@ -97,7 +95,6 @@ namespace Microsoft.CodeAnalysis.CSharp
 
             return isExtensionMember;
         }
-#nullable disable
 
         public bool IsLocalFunctionInvocation =>
             MethodGroup?.Methods.Count == 1 && // Local functions cannot be overloaded
